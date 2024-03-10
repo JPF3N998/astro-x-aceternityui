c66b9d13b0320c1108d87289d3e0293fb008e9ef
Jose Pablo Feng <jpfeng-dev@proton.me>
2024-03-09 19:26:33 -0600

    chore: Added AceternityUI dependencies
    and cn setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [addVariablesForColors],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
